a6eff6ba43416493d373ba894d1eff250aef8ea5
VISAshuttle <codemonkey@kakao.com>
2021-03-30 14:22:46 +0900

    최초 작성

---
+++ chap11Lab/src/sec07/exam01_constructor/KeyboardToStringExample.java
@@ -0,0 +1,18 @@
+package sec07.exam01_constructor;
+
+import java.io.IOException;
+
+public class KeyboardToStringExample {
+
+	public static void main(String[] args) throws IOException {
+		byte[] bytes = new byte[100];
+
+		System.out.print("입력: ");
+		int readByteLen = System.in.read(bytes);
+		System.out.println("readByteLen: " + readByteLen);
+
+		String str = new String(bytes, 0, readByteLen - 2);
+		System.out.println(str);
+	}
+
+}
