c176a55085d3b062d0f6c8124460290f52951b17
VISAshuttle <codemonkey@kakao.com>
2021-04-02 15:09:36 +0900

    git 잘못된 경로 수정

--- src/verify/exam00/Bus.java
+++
@@ -1,8 +0,0 @@
-package verify.exam00;
-public class  Bus extends Car
-{
-        private int passenger=3; //
-        public void move(){// 3
-                System.out.println("Bus  3 passenger :==> "+passenger);
-        }
-}
