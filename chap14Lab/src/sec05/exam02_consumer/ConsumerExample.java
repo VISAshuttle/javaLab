cbe27e153c85b1f52ae3e807b242e91030236b00
VISAshuttle <codemonkey@kakao.com>
2021-04-02 16:56:35 +0900

    람다식 -> 익명 구현객체

--- chap14Lab/src/sec05/exam02_consumer/ConsumerExample.java
+++ chap14Lab/src/sec05/exam02_consumer/ConsumerExample.java
@@ -7,15 +7,23 @@ import java.util.function.ObjIntConsumer;
 
 public class ConsumerExample {
 	public static void main(String[] args) {
+		/*
+		 * Consumer<String> consumer = new Consumer<String>() {
+		 * @Override public void accept(String t) { System.out.println(t + "8"); } };
+		 */
 		Consumer<String> consumer = t -> System.out.println(t + "8");
 		consumer.accept("java");
-		
-		BiConsumer<String, String> bigConsumer = (t, u) -> System.out.println(t + u);
-		bigConsumer.accept("Java", "8");
-		
+
+		/*
+		 * BiConsumer<String, String> biConsumer = new BiConsumer<String, String>() {
+		 * @Override public void accept(String t, String u) { System.out.println(t + u); } };
+		 */
+		BiConsumer<String, String> biConsumer = (t, u) -> System.out.println(t + u);
+		biConsumer.accept("Java", "8");
+
 		DoubleConsumer doubleConsumer = d -> System.out.println("Java" + d);
 		doubleConsumer.accept(8.0);
-		
+
 		ObjIntConsumer<String> objIntConsumer = (t, i) -> System.out.println(t + i);
 		objIntConsumer.accept("Java", 8);
 	}
