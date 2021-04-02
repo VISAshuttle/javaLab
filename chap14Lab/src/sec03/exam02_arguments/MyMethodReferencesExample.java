27a9c6aec54b37f3dd71e9ef2c03a261300aa971
VISAshuttle <codemonkey@kakao.com>
2021-04-02 15:41:03 +0900

    람다식 -> 익명 구현 객체 변환

--- chap14Lab/src/sec03/exam02_arguments/MyMethodReferencesExample.java
+++ chap14Lab/src/sec03/exam02_arguments/MyMethodReferencesExample.java
@@ -4,6 +4,15 @@ public class MyMethodReferencesExample {
 	public static void main(String[] args) {
 		MyFunctionalInterface fi;
 		
+		// 익명 구현 객체
+		fi = new MyFunctionalInterface() {
+			@Override
+			public void method(int x) {
+				System.out.println(x * 5);
+			}
+		};
+		fi.method(2);
+		
 		fi= (x) -> {
 			int result = x * 5;
 			System.out.println(result);
--- chap14Lab/src/sec03/exam03_return/MyFunctionalInterfaceExample.java
+++ chap14Lab/src/sec03/exam03_return/MyFunctionalInterfaceExample.java
@@ -1,37 +1,37 @@
 package sec03.exam03_return;
 
-public class MyFunctionalInterfaceExample { 
+public class MyFunctionalInterfaceExample {
 	public static void main(String[] args) {
 		MyFunctionalInterface fi;
-		
+
+		fi = new MyFunctionalInterface() {
+			@Override
+			public int method(int x, int y) {
+				int result = x + y;
+				return result;
+			}
+		};
+		fi.method(2, 5);
+
 		fi = (x, y) -> {
 			int result = x + y;
 			return result;
 		};
 		System.out.println(fi.method(2, 5));
-		
-		fi = (x, y) -> { return x + y; };
+
+		fi = (x, y) -> {
+			return x + y;
+		};
 		System.out.println(fi.method(2, 5));
-		
+
 		fi = (x, y) -> x + y;
 		System.out.println(fi.method(2, 5));
-		
+
 		fi = (x, y) -> sum(x, y);
 		System.out.println(fi.method(2, 5));
 	}
-	
+
 	public static int sum(int x, int y) {
 		return (x + y);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
